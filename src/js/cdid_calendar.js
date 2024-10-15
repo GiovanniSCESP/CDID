c88fa70eda4d0aa81f2dcbaf63dc7b2f3fc4a7a2
GiovanniSCESP <giovanniandre2003@gmail.com>
2024-10-15 11:46:35 +0100

    custom tick

--- src/js/cdid_calendar.js
+++ src/js/cdid_calendar.js
@@ -14,10 +14,12 @@ const readLocalStorage = async (key) => {
 async function main() {
     console.log('Se ejecuta main');
 
-    chrome.storage.local.get(['assignments']).then((result) => {
-        if ( !result.assignments ) {
-            chrome.storage.local.set({ 'assignments': [] });
-        }
+    chrome.storage.local.get(['assignments']).then((result) => { if ( !result.assignments ) {
+            chrome.storage.local.set({ 'assignments': [] }); }
+    });
+
+    chrome.storage.local.get(['otheractivities']).then((result) => { if ( !result.otheractivities ) {
+            chrome.storage.local.set({ 'otheractivities': [] }); }
     });
     
     eventItems = document.querySelectorAll('div.event');
--- src/js/cdid_course.js
+++ src/js/cdid_course.js
@@ -14,10 +14,12 @@ const readLocalStorage = async (key) => {
 async function main() {
     console.log('Se ejecuta main');
 
-    chrome.storage.local.get(['assignments']).then((result) => {
-        if ( !result.assignments ) {
-            chrome.storage.local.set({ 'assignments': [] });
-        }
+    chrome.storage.local.get(['assignments']).then((result) => { if ( !result.assignments ) {
+            chrome.storage.local.set({ 'assignments': [] }); }
+    });
+
+    chrome.storage.local.get(['otheractivities']).then((result) => { if ( !result.otheractivities ) {
+            chrome.storage.local.set({ 'otheractivities': [] }); }
     });
 
     var AssignActivityItems = document.querySelectorAll('li.modtype_assign div.activity-item');
@@ -35,6 +37,56 @@ async function main() {
                     assignItem.style.backgroundColor = '#cfefcf';
                 }
             });
+        }
+    }
+
+    var OtherActivityItems = document.querySelectorAll('li:not(.modtype_assign) > div.activity-item')
+
+    for ( activity of OtherActivityItems ) {
+        let check = document.createElement('input');
+        check.type = 'checkbox';
+        check.className = 'cdid-checkbox';  
+        activity.appendChild(check);
+
+        check.addEventListener('click', async (evt) => {
+            if ( evt.target.checked ) {
+                activityID = evt.target.parentElement.parentElement.dataset.id;
+                activityStatus = 1;
+                evt.target.parentElement.style.backgroundColor = '#cfefcf';
+            } else {
+                activityID = evt.target.parentElement.parentElement.dataset.id;
+                activityStatus = 0;
+                evt.target.parentElement.style.backgroundColor = 'initial';
+            }
+
+            var otheractivities = await readLocalStorage('otheractivities');
+        
+            for (let i = 0; i < otheractivities.length; i++) {
+                const oActivity = otheractivities[i];
+                
+                if ( oActivity.id == activityID ) {
+                    otheractivities.splice(i, 1);
+                }
+            }
+            
+            otheractivities.push({
+                'id': activityID,
+                'status': activityStatus
+            });
+            
+            chrome.storage.local.set({ 'otheractivities': otheractivities });
+            console.log(otheractivities);
+        })
+
+        var otheractivities = await readLocalStorage('otheractivities');
+        
+        for (let i = 0; i < otheractivities.length; i++) {
+            const oActivity = otheractivities[i];
+
+            if ( activity.parentElement.dataset.id == oActivity.id && oActivity.status ) {
+                activity.style.backgroundColor = '#cfefcf';
+                check.checked = true;
+            }
         }
     }
 }
